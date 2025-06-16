6b6edcebbd00f04e26c86ddaec7d52cfd2c8f645
Walter Pelissero <wcp@book.lan>
2025-06-16 14:09:35 +0200

    Add emty tests

---
+++ test/fourteatoo/clj_blink/api_test.clj
@@ -0,0 +1,7 @@
+(ns fourteatoo.clj-blink.api-test
+  (:require [fourteatoo.clj-blink.api :as api]
+            [clojure.test :refer :all]))
+
+(deftest a-test
+  (testing "No test.  Just compile the dependencies."
+    (is true)))
