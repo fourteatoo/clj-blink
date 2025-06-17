b535dcb7b5bba8785a61d240e712aec23329afb9
Walter Pelissero <walter@pelissero.de>
2025-06-17 18:06:18 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.fourteatoo/clj-blink "0.1.0-SNAPSHOT"
+(defproject io.github.fourteatoo/clj-blink "0.1.0"
   :description "A simple and unofficial Blink Camera API."
   :url "http://github.com/fourteatoo/clj-blink"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
