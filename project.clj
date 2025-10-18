b946fbb3a143853ace21cee729395cef18b559d6
Walter Pelissero <wcp@book>
2025-10-18 20:55:40 +0200

    Update authentication to most recent API

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.fourteatoo/clj-blink "0.1.2-SNAPSHOT"
+(defproject io.github.fourteatoo/clj-blink "1.0.0-SNAPSHOT"
   :description "A simple Blink Camera API for Clojure."
   :url "http://github.com/fourteatoo/clj-blink"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.12.1"]
                  [clojure.java-time "1.4.3"]
                  [clj-http "3.13.0"]
+                 [ring/ring-codec "1.3.0"]
                  [cheshire "6.0.0"]
                  [camel-snake-kebab "0.4.3"]]
   :profiles {:dev {:plugins [[lein-codox "0.10.8"]
